681be60bb61569a06d8d1fce57a8df582c54112f
aidadieudonne <aidaharing10@gmail.com>
2025-02-10 15:15:24 -0700

    password!

--- src/components/EyesOnMe.js
+++ src/components/EyesOnMe.js
@@ -1,1 +1,18 @@
-// Code EyesOnMe Component Here
+import React from "react";
+
+function EyesOnMe() {
+    const handleFocus = (event) => {
+        console.log('Good!')
+    };
+    const handleBlur = (event) => {
+        console.log('Hey! Eyes on me!')
+    };
+
+    return(
+        <div>
+        <button onFocus={handleFocus} onBlur={handleBlur}>Eyes on me</button>
+        </div>
+    );
+}
+
+export default EyesOnMe;
